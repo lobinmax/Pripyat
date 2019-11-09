e8acb5a007c96d7b86980abf9c9ab830efa111e3
m-lobin <52151502+m-lobin@users.noreply.github.com>
2019-11-09 17:40:28 +0700

    Add files via upload

---
+++ pRScripts/Triggers/dbo.Pr_tgrAbonents.Trigger.sql
@@ -0,0 +1,13 @@
+﻿IF EXISTS (SELECT name FROM sysobjects WHERE  name = N'Pr_tgrAbonents' AND type = 'TR')
+    DROP TRIGGER Pr_tgrAbonents
+GO
+CREATE TRIGGER Pr_tgrAbonents
+	ON Abonents
+	FOR INSERT, DELETE
+AS BEGIN
+	-- после добавления
+	INSERT INTO Pr_Abonents
+	SELECT i.AbonentId FROM INSERTED i
+	-- после удаления
+	DELETE Pr_Abonents WHERE AbonentId = (SELECT d.AbonentId FROM DELETED d)
+END 
---
+++ pRScripts/Triggers/dbo.Pr_tgrPoints.Trigger.sql
@@ -0,0 +1,13 @@
+﻿IF EXISTS (SELECT name FROM sysobjects WHERE  name = N'Pr_tgrPoints' AND type = 'TR')
+    DROP TRIGGER Pr_tgrPoints
+GO
+CREATE TRIGGER Pr_tgrPoints
+	ON Points
+	FOR INSERT, DELETE
+AS BEGIN
+	-- после добавления
+	INSERT INTO Pr_Points
+	SELECT i.PointId FROM INSERTED i
+	-- после удаления
+	DELETE Pr_Points WHERE PointId = (SELECT d.PointId FROM DELETED d)
+END 
